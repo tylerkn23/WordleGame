9d8548dcab4f68c89a4085c6d9265113c08dc8f1
Tyler <tylerkn23@sherwoodsd.com>
2023-01-09 12:03:48 -0800

    6 letter text file

---
+++ src/WordleGame.java
@@ -0,0 +1,11 @@
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class WordleGame implements PropertyChangeListener {
+
+    
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
