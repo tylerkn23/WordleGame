46fffd4a5bd6be1132ebef27839823af7cbba40e
Tyler <tylerkn23@sherwoodsd.com>
2023-01-10 12:33:29 -0800

    carrying cyrus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                runGUI();
+            }
+        });
+    }
+
+    public static void runGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        WordleGame game = new WordleGame();
     }
 }
--- src/WordleGame.java
+++ src/WordleGame.java
@@ -1,9 +1,23 @@
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
 public class WordleGame implements PropertyChangeListener {
 
-    
+    JFrame frame;
+    JLabel label, label1;
+    JPanel contentPane;
+    JButton button;
+
+    public WordleGame() {
+        frame = new JFrame("DivisibleBy3");
+        contentPane = new JPanel();
+        label = new JLabel("Enter an integer: ");
+        label1 = new JLabel();
+        button = new JButton("Check");
+    }
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
 
