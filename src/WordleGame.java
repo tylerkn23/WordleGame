666791f1e8df784de0355e76b6ecae29a9cfd0bf
Tyler <tylerkn23@sherwoodsd.com>
2023-01-10 12:36:22 -0800

    carrying cyrus 2.0

--- src/WordleGame.java
+++ src/WordleGame.java
@@ -10,6 +10,7 @@ public class WordleGame implements PropertyChangeListener {
     JLabel label, label1;
     JPanel contentPane;
     JButton button;
+    JTextField input;
 
     public WordleGame() {
         frame = new JFrame("DivisibleBy3");
@@ -17,6 +18,19 @@ public class WordleGame implements PropertyChangeListener {
         label = new JLabel("Enter an integer: ");
         label1 = new JLabel();
         button = new JButton("Check");
+        input = new JTextField(5);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setContentPane(contentPane);
+
+        contentPane.add(label);
+        contentPane.add(input);
+        contentPane.add(button);
+        contentPane.add(label1);
+
+        contentPane.setLayout(new GridLayout(2, 2, 50, 10));
     }
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
